70151ad5c4ea09e17ee314e096d7dc9b0be05fae
Simon <simon@7lieues.io>
2023-08-12 17:30:28 +0200

    feat: adds tag indication if filtered page

--- lib/nimble_blog_web/controllers/blog_controller.ex
+++ lib/nimble_blog_web/controllers/blog_controller.ex
@@ -3,10 +3,10 @@ defmodule NimbleBlogWeb.BlogController do
 
   alias NimbleBlog.Blog
 
-  def index(conn, _params), do: render(conn, "index.html", posts: Blog.all_posts())
+  def index(conn, _params), do: render(conn, "index.html", posts: Blog.all_posts(), filter: false)
 
   def show(conn, %{"id" => id}), do: render(conn, "show.html", post: Blog.get_post_by_id!(id))
 
   def tag_filter(conn, %{"tag" => tag}),
-    do: render(conn, "index.html", posts: Blog.get_posts_by_tag!(tag))
+    do: render(conn, "index.html", posts: Blog.get_posts_by_tag!(tag), filter: tag)
 end
