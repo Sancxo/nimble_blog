c11ef56c03865d9dae237bf2f2f0b913a77da38c
Simon <simon@7lieues.io>
2023-08-13 17:30:57 +0200

    fix: removes warning in unused default function

--- lib/nimble_blog_web/controllers/page_controller.ex
+++ lib/nimble_blog_web/controllers/page_controller.ex
@@ -4,7 +4,7 @@ defmodule NimbleBlogWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    redirect(conn, to: ~p"/blog")
+    redirect(conn, to: ~p"/")
     # render(conn, :home, layout: false)
   end
 end
