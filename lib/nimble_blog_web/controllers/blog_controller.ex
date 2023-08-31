bce08bf09710edf126f0bfcda046ca49a5e746e6
Simon <simon@7lieues.io>
2023-08-31 12:37:02 +0200

    refactor: removes doubles article in index route controller whe nan article is available both in english and in french (english is kept)

--- lib/nimble_blog_web/controllers/blog_controller.ex
+++ lib/nimble_blog_web/controllers/blog_controller.ex
@@ -3,8 +3,20 @@ defmodule NimbleBlogWeb.BlogController do
 
   alias NimbleBlog.Blog
 
-  def index(conn, _params),
-    do: render(conn, "index.html", posts: Blog.all_posts(), tag_filter: "all", lang_filter: "all")
+  def index(conn, _params) do
+    posts =
+      Blog.all_posts()
+      |> Enum.reduce([], fn
+        post, [head | _tail] = acc ->
+          if head.date == post.date, do: acc, else: [post | acc]
+
+        post, acc ->
+          [post | acc]
+      end)
+      |> Enum.reverse()
+
+    render(conn, "index.html", posts: posts, tag_filter: "all", lang_filter: "all")
+  end
 
   def show(conn, %{"id" => id}),
     do:
