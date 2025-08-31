306de2dd5d81f346fe1d2f2cf73196edf3b194f2
Simon Tirant <simon.tirant@gmail.com>
2025-09-01 01:09:57 +0200

    fix: uses send_download to return a downloaded JSON for webfinger protocol

--- lib/nimble_blog_web/controllers/activity_pub_controller.ex
+++ lib/nimble_blog_web/controllers/activity_pub_controller.ex
@@ -2,6 +2,18 @@ defmodule NimbleBlogWeb.ActivityPubController do
   use NimbleBlogWeb, :controller
 
   def webfinger(conn, _) do
-    render(conn, :webfinger)
+    send_download(conn, {:binary, %{
+        subject: "acct:@blog@blog.simontirant.dev",
+        aliases: [
+          "https://blog.simontirant.dev/@blog"
+        ],
+        links: [
+          %{
+            rel: "self",
+            type: "application/activity+json",
+            href: "https://blog.simontirant.dev/@blog"
+          }
+        ]
+    } |> Jason.encode!()}, filename: "webfinger")
   end
 end
