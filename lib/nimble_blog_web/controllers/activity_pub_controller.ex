8897ef167357a4bdcfe09647c2105decccdd40e1
Simon Tirant <simon.tirant@gmail.com>
2025-09-01 00:39:38 +0200

    fix: adds Jason.encode! function in order to encode JSON binary

--- lib/nimble_blog_web/controllers/activity_pub_controller.ex
+++ lib/nimble_blog_web/controllers/activity_pub_controller.ex
@@ -2,19 +2,21 @@ defmodule NimbleBlogWeb.ActivityPubController do
   use NimbleBlogWeb, :controller
 
   def webfinger(conn, _) do
-    json = %{
-      subject: "acct:@blog@blog.simontirant.dev",
-      aliases: [
-        "https://blog.simontirant.dev/@blog"
-      ],
-      links: [
-        %{
-          rel: "self",
-          type: "application/activity+json",
-          href: "https://blog.simontirant.dev/@blog"
-        }
-      ]
-    }
+    json =
+      %{
+        subject: "acct:@blog@blog.simontirant.dev",
+        aliases: [
+          "https://blog.simontirant.dev/@blog"
+        ],
+        links: [
+          %{
+            rel: "self",
+            type: "application/activity+json",
+            href: "https://blog.simontirant.dev/@blog"
+          }
+        ]
+      }
+      |> Jason.encode!()
 
     send_download(conn, {:binary, json})
   end
