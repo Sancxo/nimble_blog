f74b477ae484324274bc13257b0eeb95f480fb61
Simon Tirant <simon.tirant@gmail.com>
2025-09-01 00:51:33 +0200

    fixes: uses a view to render JSON data

--- lib/nimble_blog_web/controllers/activity_pub_controller.ex
+++ lib/nimble_blog_web/controllers/activity_pub_controller.ex
@@ -2,22 +2,6 @@ defmodule NimbleBlogWeb.ActivityPubController do
   use NimbleBlogWeb, :controller
 
   def webfinger(conn, _) do
-    json =
-      %{
-        subject: "acct:@blog@blog.simontirant.dev",
-        aliases: [
-          "https://blog.simontirant.dev/@blog"
-        ],
-        links: [
-          %{
-            rel: "self",
-            type: "application/activity+json",
-            href: "https://blog.simontirant.dev/@blog"
-          }
-        ]
-      }
-      |> Jason.encode!()
-
-    send_download(conn, {:binary, json})
+    render(conn, :webfinger)
   end
 end
