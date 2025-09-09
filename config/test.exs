5efea57a40f3ad85405ee05b4796a0e1a9b4f9c2
Simon Tirant <simon.tirant@gmail.com>
2025-09-09 23:01:13 +0200

    chore: mix format

--- config/test.exs
+++ config/test.exs
@@ -8,8 +8,7 @@ config :nimble_blog, NimbleBlogWeb.Endpoint,
   server: false
 
 # In test we don't send emails.
-config :nimble_blog, NimbleBlog.Mailer,
-  adapter: Swoosh.Adapters.Test
+config :nimble_blog, NimbleBlog.Mailer, adapter: Swoosh.Adapters.Test
 
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
